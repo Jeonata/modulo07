364435d4058eaee43f6434bcd22f7a8476a8f6b4
Jeonata <jeonata@gmail.com>
2019-09-15 15:30:17 -0300

    Styles: Global Styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyles from './styles/global';
 import Routes from './routes';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       {/* <Header /> */}
       <Routes />
+      <GlobalStyles />
     </BrowserRouter>
   );
 }
