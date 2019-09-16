485081b816d8c7006cddba3d6dd2a42fcb9c4cfc
Jeonata <jeonata@gmail.com>
2019-09-15 22:46:18 -0300

    Chore: Calculate and display quantity

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -32,6 +32,8 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
+
     return (
       <ProductList>
         {products.map(product => (
@@ -45,9 +47,8 @@ class Home extends Component {
               onClick={() => this.handleAddProduct(product)}
             >
               <div>
-                <MdAddShoppingCart size={16} color="#FFF">
-                  3
-                </MdAddShoppingCart>
+                <MdAddShoppingCart size={16} color="#FFF" />
+                {amount[product.id] || 0}
               </div>
               <span>ADICIONAR AO CARRINHO</span>
             </button>
@@ -58,10 +59,17 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+    return amount;
+  }, {}),
+});
+
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Home);
